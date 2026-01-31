41034f58b566180308c38664ecb141ca953506fb
railzen <railzen@126.com>
2026-01-31 14:55:48 +0800

    [CHG]更新XANMOD安装

--- ludo.sh
+++ ludo.sh
@@ -1681,19 +1681,17 @@ system_tools_menu() {
 
 				new_swap=510
 				add_swap
-				install wget gnupg
+				install wget gpg ca-certificates lsb-release apt-transport-https
 
 				wget -qO - https://dl.xanmod.org/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg --yes
-				# wget -qO - https://raw.githubusercontent.com/railzen/CherryScript/main/tools/config/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg --yes
 
 				# 步骤3：添加存储库
 				echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
 
 				version=$(wget -q https://dl.xanmod.org/check_x86-64_psabi.sh && chmod +x check_x86-64_psabi.sh && ./check_x86-64_psabi.sh | grep -oP 'x86-64-v\K\d+|x86-64-v\d+')
-				# version=$(wget -q https://raw.githubusercontent.com/railzen/CherryScript/main/tools/check_x86-64_psabi.sh && chmod +x check_x86-64_psabi.sh && ./check_x86-64_psabi.sh | grep -oP 'x86-64-v\K\d+|x86-64-v\d+')
 
-				apt update -y
-				apt install -y linux-xanmod-x64v$version
+				apt-get update
+				apt-get install -y linux-xanmod-x64v$version
 
 				# 步骤5：启用BBR3
 				cat >/etc/sysctl.conf <<EOF
